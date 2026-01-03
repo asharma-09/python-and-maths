89dd10415766297d4e5171b14f1af8a7426317ac
Avani Sharma <avnai.sharma@gmail.com>
2026-01-03 18:59:35 +0530

    Added Fibonacci series generator

---
+++ series-generator.py
@@ -0,0 +1,12 @@
+import os
+
+# Generate Fibonacci series up to n terms
+
+n = int(input("Enter number of terms: "))
+
+a, b = 0, 1
+
+print("Fibonacci Series:")
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
